0cdf4196c691ffc0330443af497bf08cc7880bea
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-13 01:25:37 -0800

    varable and data type

---
+++ C++/variable and data type.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;\
+
+int main(){
+	int age =20;
+	float speed =50.5;
+	double height =1.75;
+	char alp ='E';
+	bool isTrue = true;
+	string name ="Elshady";
+	
+	cout<<age<<endl;
+	cout<<speed<<endl;
+	cout<<height<<endl;
+	cout<<alp<<endl;
+	cout<<isTrue<<endl;
+	cout<<name<<endl;
+	return 0;
+}
