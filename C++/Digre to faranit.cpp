861b1b61dade0cc42656969acffd874b32230d31
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-13 02:34:38 -0800

    Digre to faranit

---
+++ C++/Digre to faranit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float digreCelcias;
+	cout<<"Inter Degricelcias: ";
+	cin>>digreCelcias;
+	float faranit =( digreCelcias*1.8)+32;
+	cout<<"Degre Faranit is: "<<faranit<<endl;
+	return 0;
+	
+}
