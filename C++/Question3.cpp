e90fc7ebb646faa7adcf21edc52af386f8ec450b
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-13 03:01:47 -0800

    Question3

--- C++/Question3.cpp
+++ C++/Question3.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main(){
+	const float pi =3.14;
 	float radios;
 	cout<<"Enter Radios: ";
 	cin>>radios;
-	radios = 2*3.14*radios;
+	radios = 2*pi*radios;
 	cout<<"Circumfrance is: "<<radios<<endl;
 	return 0;
 	
