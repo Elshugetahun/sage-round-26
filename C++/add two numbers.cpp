e2992c319891a606899b3cad5081beb856d637d1
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-13 02:26:15 -0800

    two number

---
+++ C++/add two numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num1,num2,sum;
+	cout<<"Enter first number: ";
+	cin>>num1;
+	cout<<"Enter secund number: ";
+	cin>>num2;
+	sum=num1+num2;
+	cout<<"The result is "<<sum<<endl;
+	return 0;
+}
