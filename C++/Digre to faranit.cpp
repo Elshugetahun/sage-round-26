15f8213eaf1ab02b25b5a4e02fbeee0432d03474
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-13 02:41:43 -0800

    Question1

--- C++/Digre to faranit.cpp
+++ C++/Digre to faranit.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
 	float digreCelcias;
-	cout<<"Inter Degricelcias: ";
+	cout<<"Enter Degricelcias: ";
 	cin>>digreCelcias;
-	float faranit =( digreCelcias*1.8)+32;
+	float faranit =(digreCelcias*1.8)+32;
 	cout<<"Degre Faranit is: "<<faranit<<endl;
 	return 0;
 	
