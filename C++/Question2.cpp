bb8b9b65e233c6df4e7963c03055c1333d25e0bf
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-13 02:53:21 -0800

    Question2

---
+++ C++/Question2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string name;
+	int age;
+	cout<<"Enter your Name: ";
+	cin>>name;
+	cout<<"Enter your Age: ";
+	cin>>age;
+	cout<<"your name is: "<<name<<endl;
+	cout<<"your Age is: "<<age;
+	return 0;
+}
