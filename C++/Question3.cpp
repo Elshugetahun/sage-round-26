c1f213d6442f936fe01561c75d501ca2faead6ea
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-13 02:59:42 -0800

    Question3

---
+++ C++/Question3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float radios;
+	cout<<"Enter Radios: ";
+	cin>>radios;
+	radios = 2*3.14*radios;
+	cout<<"Circumfrance is: "<<radios<<endl;
+	return 0;
+	
+}
