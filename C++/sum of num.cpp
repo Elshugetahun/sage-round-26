2d227d9139dbb63f317a284e31fae5f3f4fcc34d
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-18 02:49:04 -0800

    sum of array number

---
+++ C++/sum of num.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num1[3] ={12,34,5};
+	int num2[3] ={34,1,5};
+	int num3[3] ={5,78,9};
+	int sumofnum1;
+	int sumofnum2;
+	int sumofnum3;
+	
+	for(int i=0;i<3;i++){
+		sumofnum1=sumofnum1+num1[i];
+	}
+	for(int i=0;i<3;i++){
+		sumofnum2=sumofnum1+num2[i];
+	}
+	for(int i=0;i<3;i++){
+		sumofnum3=sumofnum3+num3[i];
+	}
+
+
+cout<<"sum of num 1: "<<sumofnum1<<endl;
+cout<<"sum of num 2: "<<sumofnum2<<endl;
+cout<<"sum of num 3: "<<sumofnum3<<endl;
+
+
+}
