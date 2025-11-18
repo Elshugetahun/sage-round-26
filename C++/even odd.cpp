55d1e99260c9fd3ae72639c0bce44630d791476e
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-18 03:02:26 -0800

    even odd

---
+++ C++/even odd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num[7];
+	int even;
+	int odd;
+	
+	for(int i=0;i<7;i++){
+		cin>>num[i];
+	}
+	if(num[i]%2==0){
+		cout<<"even";
+	}else{
+		cout<<"odd";
+	}
+
+
+}
