8f7c583a8549569bee07141ec2cf7e1bae8f014e
elshugetahun@gmail.com <elshugetahun@gmail.com>
2025-11-18 01:11:04 -0800

    arry index loop

---
+++ C++/array index loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int marks[5];
+	for(int i=0;i<5;i++){
+		cin>>marks[i];
+	}
+	cout<<"You enterd"<<endl;
+	for(int i=0;i<5;i++)
+	cout<<marks[i]<<endl;
+	}
