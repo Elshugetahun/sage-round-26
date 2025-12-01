49491830a465ab5f23b3e5fd7fc6236adf2b4486
meron <meriprizerihun@gmail.com>
2025-12-01 00:13:48 -0800

    javascript

---
+++ JAVA SCRIPT/script.js
@@ -0,0 +1,42 @@
+   var x=1;
+    let y=3;
+        // console.log(x+y);
+       // console.log("Hello");
+
+    function sayHello(){
+        alert('Hello form java')
+    }
+
+    function sum(x,y){
+        return x+y;
+    }
+
+    // sayHello();
+
+    // let z=sum(1,65);
+
+    // console.log(z);
+    
+    // let age=15;
+
+    // if(age>=18){
+    //     console.log("You are An Adult")
+    // }else{
+    //     console.log("You are a Miner")
+    // }
+    // console.log("Loop")
+    // for(let i=1;i<=5;i++){
+    //     console.log(i);
+    // }
+    // console.log("while")
+    // let i=10;
+
+    // while(i>=1){
+    //     console.log(i);
+    //     i--
+    // }
+    function changeText(){
+        let title=document.getElementById('title');
+        title.innerText="New Text"
+    }
+    
