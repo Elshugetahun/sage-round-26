b812ca27aba8f09cd5b6eb7b6abfb089d114899f
Elshugetahun <elshugetahun@gmail.com>
2025-12-04 14:05:50 +0300

    java DOM

---
+++ JAVA SCRIPT/javascript 2/script.js
@@ -0,0 +1,15 @@
+document.getElementById('demo').innerText="hi from java script";
+document.getElementById('demo').innerHTML="<h1>Hello</h1>";
+document.getElementById('demo').style.color="red"
+
+let x;
+let y;
+let z;
+
+x=20;
+y=5;
+z=x+y;
+console.log(z);
+
+document.getElementById('result').style.fontSize="100px";
+document.getElementById('result').innerHTML=z;
